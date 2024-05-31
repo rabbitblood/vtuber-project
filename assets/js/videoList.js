2edbce7338537f90f4e0c820c5c53cd503da80e8
rabbitblood <qz154772610@gmail.com>
2024-05-31 01:31:41 -0700

    init video list

---
+++ assets/js/videoList.js
@@ -0,0 +1,60 @@
+{
+  //     <!-- fifth section -->
+  //     <div class="videos-section-container">
+  //       <div class="videos-container">
+  //         <div class="vtuber-video">
+  //           <img src="https://placehold.co/400" alt="" />
+  //           <div class="video-title">视频标题</div>
+  //         </div>
+  //         <div class="vtuber-video">
+  //           <img src="https://placehold.co/400" alt="" />
+  //           <div class="video-title">视频标题</div>
+  //         </div>
+  //         <div class="vtuber-video">
+  //           <img src="https://placehold.co/400" alt="" />
+  //           <div class="video-title">视频标题</div>
+  //         </div>
+  //       </div>
+  //       <div class="button refresh-video">刷新图片</div>
+  //     </div>
+  //   </section>
+}
+const videoData = {
+  videos: {
+    video1: {
+      title: "video1",
+      url: "https://www.youtube.com/watch?v=XruowTM5Uyg",
+      thumbnail: "https://i3.ytimg.com/vi/XruowTM5Uyg/hqdefault.jpg",
+    },
+    video2: {
+      title: "video2",
+      url: "https://www.youtube.com/watch?v=xFf9FrbwY-M",
+      thumbnail: "https://i3.ytimg.com/vi/xFf9FrbwY-M/hqdefault.jpg",
+    },
+    video3: {
+      title: "video3",
+      url: "https://www.youtube.com/watch?v=9srTa3GgGQ0",
+      thumbnail: "https://i3.ytimg.com/vi/9srTa3GgGQ0/hqdefault.jpg",
+    },
+    video4: {
+      title: "video4",
+      url: "https://www.youtube.com/watch?v=OzBpGYAISvw",
+      thumbnail: "https://i3.ytimg.com/vi/OzBpGYAISvw/hqdefault.jpg",
+    },
+    video5: {
+      title: "video5",
+      url: "https://www.youtube.com/watch?v=9srTa3GgGQ0",
+      thumbnail: "https://i3.ytimg.com/vi/9srTa3GgGQ0/hqdefault.jpg",
+    },
+    video6: {
+      title: "video6",
+      url: "https://www.youtube.com/watch?v=29WAdrzHSJo",
+      thumbnail: "https://i3.ytimg.com/vi/29WAdrzHSJo/hqdefault.jpg",
+    },
+    video7: {
+      title: "video7",
+      url: "https://www.youtube.com/watch?v=JaLt3o0SmcE",
+      thumbnail: "https://i3.ytimg.com/vi/JaLt3o0SmcE/hqdefault.jpg",
+    },
+  },
+};
